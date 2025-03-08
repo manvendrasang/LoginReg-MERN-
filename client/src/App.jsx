a8abdbca9c4a6c1be3b8ca5fc08a0e90e32b6241
Manvendra <manvendrasang@gmail.com>
2025-03-08 13:16:53 +0530

    Set up the starting text for login and register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
 
   return (
     <>
-    <h1>Hello</h1>
+    <Navbar />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/login' element={<Login />} />
+    </Routes>
     </>
   )
 }
