bde6e6e019c10c4b0cc606d6433ed475d89a7934
Manvendra <manvendrasang@gmail.com>
2025-03-09 14:53:29 +0530

    done hashing of passwords

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,6 @@
 const User = require('../models/user')
+const { hashPassword, comparePassword} = require('../helpers/auth')
+
 const test = (req, res) => {
     res.json('Test is working')
 }
@@ -29,8 +31,13 @@ const registerUser = async (req, res) => {
             })
         };
         
+        const hashedPassword = await hashPassword(password)
+
+        //create user in database
         const user = await User.create({
-            name, email, password
+            name, 
+            email, 
+            password: hashedPassword,
         })
 
         return res.json(user)
